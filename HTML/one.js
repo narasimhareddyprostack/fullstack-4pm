a3825400974e90a0ca8162abe5d57524f7d29213
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2021-02-13 19:10:57 +0530

    git push

--- HTML/one.js
+++ HTML/one.js
@@ -1,1 +1,20 @@
-console.log('Good Evening')
+/* function add() {
+  console.log("Call me ");
+}
+
+add();
+function add() {
+  console.log("Call me Again");
+}
+add();
+ */
+
+var add = () => {
+  console.log("Call me ");
+};
+add();
+var add = () => {
+  console.log("Call me Again ");
+};
+
+add();
